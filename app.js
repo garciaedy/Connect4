139a84705eb4b89594aa603cfea72b65f02abc3d
CB_ATL_120_Edy_Garcia <edygarcia97@gmail.com>
2018-12-18 11:46:33 -0500

    js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // set up variables
-let playeOne = (s('.playerOne .name')).eq(0).text()
-let playeTwo = (s('.playerTwo .name')).eq(0).text()
-let playerScoreOne = 0;
-let playerScoreTwo = 0;
+// let playeOne = (s('.playerOne .name')).eq(0).text()
+// let playeTwo = (s('.playerTwo .name')).eq(0).text()
+// let playerScoreOne = 0;
+// let playerScoreTwo = 0;
+
+var column = $(".col");
+// declare player name by color chosen 
+var curPlayer ="skyblue";
+var curPlayer = "salmon";
+// declaring jquery selector for the grid
+var grid = $(".grid");
+var dots = $("#dots");
+var winner = $("#winner");
+
+$(document).on("mouseover", function (){
+dots.css({ display: "block", "background-color": curPlayer});
+})
+
